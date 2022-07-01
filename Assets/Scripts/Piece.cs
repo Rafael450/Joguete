68fcaec8de35a2262b3feefbaba40f7d7d9affbf
Amita-Amita <ymartins1999@gmail.com>
2022-07-01 01:13:01 -0300

    tabulero funcionano vamo ve se o git deixa

---
+++ Assets/Scripts/Piece.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Piece : MonoBehaviour 
+{
+
+    Turnos turnos;
+    public string time;
+    public int moves = 0;
+
+    void Start()
+    {
+        turnos = GameObject.FindWithTag("Turnos").GetComponent<Turnos>();
+    }
+
+    void ChoosePiece()
+    {
+        if(turnos.dieResult != 0 && time == turnos.player.tag)
+        {
+            moves = turnos.dieResult;
+            turnos.dieResult = 0;
+            turnos.NextPlayer();
+        }
+    }
+
+}
