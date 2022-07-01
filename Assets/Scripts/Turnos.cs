809b91bc318538a8dae37d2ffcca18d5ce0c9953
Rafael450 <rafaelgamer450@gmail.com>
2022-07-01 00:49:41 -0300

    feat: added turns script and a piece spript

---
+++ Assets/Scripts/Turnos.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turnos : MonoBehaviour
+{
+    int playerIndex = 0;
+    public GameObject player;
+    public List<GameObject> players = new List<GameObject>();
+    public int dieResult = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void NextPlayer()
+    {
+        playerIndex++;
+        if (playerIndex >= players.Count)
+        {
+            playerIndex = 0;
+        }
+        player = players[playerIndex];
+    }
+
+    public void RollDie()
+    {
+        dieResult = Random.Range(1, 7);
+    }
+}
